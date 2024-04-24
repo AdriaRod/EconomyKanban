2e995432d17b0526e13369b8f1255c4e6cd9465e
gabrielfonseca333 <gabrielfonsecag@gmail.com>
2024-04-24 21:34:37 +0200

    Add splash, welcome and register(updated)

---
+++ app/src/main/java/com/econok/economykanban/WelcomeActivity.java
@@ -0,0 +1,62 @@
+package com.econok.economykanban;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class WelcomeActivity extends AppCompatActivity {
+
+    private Button btn_login, btn_register;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_welcome);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        btn_register = findViewById(R.id.btn_register);
+        btn_register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                goToRegisterActivity();
+            }
+        });
+
+        btn_login = findViewById(R.id.btn_login);
+        btn_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                goToLoginActivity();
+            }
+        });
+
+
+    }//fin del OnCreate
+
+    private void goToRegisterActivity(){
+
+        Intent intent = new Intent(WelcomeActivity.this, Register.class);
+        startActivity(intent);
+        finish();
+    }
+
+    private void goToLoginActivity(){
+
+        Intent intent = new Intent(WelcomeActivity.this, Login.class);
+        startActivity(intent);
+        finish();
+    }
+
+}
