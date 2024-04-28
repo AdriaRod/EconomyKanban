4cb61ba6dd9e82e4486f695add4f828399eab450
Cumcerberus <megagodxxx@gmail.com>
2024-04-28 17:54:11 +0200

    cambios de main a central

--- app/src/main/java/com/econok/economykanban/Central.java
+++ app/src/main/java/com/econok/economykanban/Central.java
@@ -1,12 +1,19 @@
 package com.econok.economykanban;
 
+import static android.content.ContentValues.TAG;
+
 import android.graphics.Color;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
 import android.view.MenuItem;
 
 import androidx.activity.EdgeToEdge;
 import androidx.annotation.IdRes;
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -18,27 +25,107 @@ import androidx.viewpager.widget.ViewPager;
 import android.view.MenuItem;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Toast;
 
 //import com.econok.economykanban.databinding.ActivityCentralBinding;
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.target.CustomTarget;
+import com.bumptech.glide.request.transition.Transition;
 import com.econok.economykanban.fragments.SectionsPagerAdapter;
 import com.econok.economykanban.fragments.TransactionsFragment;
 import com.google.accompanist.systemuicontroller.SystemUiController;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.material.bottomnavigation.BottomNavigationItemView;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.appcheck.FirebaseAppCheck;
+import com.google.firebase.appcheck.playintegrity.PlayIntegrityAppCheckProviderFactory;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 
 
 public class Central extends AppCompatActivity {
 
     private SectionsPagerAdapter sectionsPagerAdapter;
     private MenuItem prevMenuItem;
+    private FirebaseAuth mAuth;
+    private FirebaseFirestore mFirestore;
+    private Drawable profileImage;
+    private GoogleSignInClient mGoogleSignInClient;
+    private boolean doubleBackToExitPressedOnce = false;
 
-//    ActivityCentralBinding binding;
+  //  ActivityCentralBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_central);
+
+        //FIREBASE
+        mFirestore= FirebaseFirestore.getInstance();
+        mAuth = FirebaseAuth.getInstance();
+
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken("606138593322-qmo8r77q8faabttijt0tj9e6aiai0rtm.apps.googleusercontent.com")
+                .requestEmail()
+                .build();
+
+        mGoogleSignInClient = GoogleSignIn.getClient(this,gso);
+
+        FirebaseApp.initializeApp(/*context=*/ this);
+        FirebaseAppCheck firebaseAppCheck = FirebaseAppCheck.getInstance();
+        firebaseAppCheck.installAppCheckProviderFactory(
+                PlayIntegrityAppCheckProviderFactory.getInstance());
+
+        // Obtener el usuario actual
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+
+        // Verificar si el usuario está autenticado
+        if (currentUser != null) {
+            // Obtener el nombre de usuario (este sería el nombre de la imagen en Storage)
+            String userName = currentUser.getUid(); // o el campo que contenga el nombre único
+
+            // Construir la referencia a la imagen en Firebase Storage
+            StorageReference profileImageRef = FirebaseStorage.getInstance().getReference().child("perfil/").child(userName);
+
+            // Descargar la imagen y establecerla como ícono del perfil en el menú
+            profileImageRef.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                @Override
+                public void onSuccess(Uri uri) {
+                    // Utilizar Glide u otra biblioteca para cargar la imagen desde la URL y establecerla en el ícono del menú
+                    Glide.with(getApplicationContext())
+                            .load(uri)
+                            .circleCrop()
+                            .into(new CustomTarget<Drawable>() {
+                                @Override
+                                public void onResourceReady(@NonNull Drawable resource, @Nullable Transition<? super Drawable> transition) {
+                                    // Establecer la imagen como ícono del perfil en el menú
+                                    profileImage=resource;
+                                    invalidateOptionsMenu();
+                                }
+
+                                @Override
+                                public void onLoadCleared(@Nullable Drawable placeholder) {
+                                    // No es necesario hacer nada aquí
+                                }
+                            });
+                }
+            }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    // Manejar el caso en el que no se pueda descargar la imagen
+                    Log.e(TAG, "Error al descargar la imagen del perfil: " + e.getMessage());
+                }
+            });
+        }
 
         //1
 
@@ -113,6 +200,29 @@ public class Central extends AppCompatActivity {
 
 
     }//final del OnCreate
+
+
+
+    @Override
+    public void onBackPressed() {
+        if (doubleBackToExitPressedOnce) {
+            super.onBackPressed();
+            moveTaskToBack(true);
+            return;
+        }
+
+        this.doubleBackToExitPressedOnce = true;
+        Toast.makeText(Central.this, "Presiona ATRÁS de nuevo para salir", Toast.LENGTH_SHORT).show();
+
+        new Handler().postDelayed(new Runnable() {
+
+            @Override
+            public void run() {
+                doubleBackToExitPressedOnce=false;
+            }
+        }, 2000);
+    }
+
 
     //5
     public static void removeBadge(BottomNavigationView bottomNavigationView, @IdRes int itemId) {
