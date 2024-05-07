3522d24d205f26d5a5780724d4a334ad1f3fd281
AdriaRod <adriarodriguez34@gmail.com>
2024-05-07 21:10:25 +0200

    Created GraphicView and added months, and set current date

--- app/src/main/java/com/econok/economykanban/fragments/GraphicsFragment.java
+++ app/src/main/java/com/econok/economykanban/fragments/GraphicsFragment.java
@@ -1,66 +1,126 @@
 package com.econok.economykanban.fragments;
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
 
 import com.econok.economykanban.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link GraphicsFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import java.text.DateFormatSymbols;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
 public class GraphicsFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    private TextView currentDateTextView;
+    private RadioButton previousMonthButton;
+    private RadioButton currentMonthButton;
+    private RadioButton nextMonthButton;
+    private int currentMonthIndex = 5; // Junio por defecto
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private ImageView nextButton, previousButton;
 
     public GraphicsFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment GraphicsFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static GraphicsFragment newInstance(String param1, String param2) {
-        GraphicsFragment fragment = new GraphicsFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_graphics, container, false);
+        View view = inflater.inflate(R.layout.fragment_graphics, container, false);
+        currentDateTextView = view.findViewById(R.id.titleTextView);
+
+        previousMonthButton = view.findViewById(R.id.previous_month);
+        currentMonthButton = view.findViewById(R.id.current_month);
+        nextMonthButton = view.findViewById(R.id.next_month);
+
+        previousButton = view.findViewById(R.id.previousButton);
+        nextButton = view.findViewById(R.id.nextButton);
+        updateMonthsText();
+
+        previousMonthButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                currentMonthIndex--;
+                if (currentMonthIndex < 0) currentMonthIndex = 11;
+                updateMonthsText();
+            }
+        });
+
+        nextMonthButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                currentMonthIndex++;
+                if (currentMonthIndex > 11) currentMonthIndex = 0;
+                updateMonthsText();
+            }
+        });
+
+        previousButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                currentMonthIndex--;
+                if (currentMonthIndex < 0) currentMonthIndex = 11;
+                updateMonthsText();
+            }
+        });
+
+        nextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                currentMonthIndex++;
+                if (currentMonthIndex > 11) currentMonthIndex = 0;
+                updateMonthsText();
+            }
+        });
+
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        //*********************** CURRENT DATE *******************
+        Date currentDate = new Date();
+        int v_date = R.string.date_format;
+
+        SimpleDateFormat dateFormat = new SimpleDateFormat(getString(v_date), Locale.getDefault());
+        String formattedDate = dateFormat.format(currentDate);
+
+        String capitalizedDate = formattedDate.substring(0, 1).toUpperCase() + formattedDate.substring(1);
+        currentDateTextView.setText(capitalizedDate);
+    }
+
+    private void updateMonthsText() {
+        int previousMonthIndex = currentMonthIndex - 1;
+        int nextMonthIndex = currentMonthIndex + 1;
+
+        if (previousMonthIndex < 0) previousMonthIndex = 11;
+        if (nextMonthIndex > 11) nextMonthIndex = 0;
+
+        previousMonthButton.setText(getMonthAbbreviation(previousMonthIndex));
+        currentMonthButton.setText(getMonthAbbreviation(currentMonthIndex));
+        nextMonthButton.setText(getMonthAbbreviation(nextMonthIndex));
+
+        previousMonthButton.setChecked(false);
+        currentMonthButton.setChecked(true);
+        nextMonthButton.setChecked(false);
+    }
+
+    private String getMonthAbbreviation(int month) {
+        DateFormatSymbols dfs = new DateFormatSymbols();
+        String[] months = dfs.getShortMonths();
+        return months[month];
     }
 }
